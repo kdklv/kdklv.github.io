e403d246700ed8dcdec41df7e760eb1260884bc9
Kristjan Dekleva <you@example.com>
2018-11-07 13:11:20 +0100

    triangulate

--- txt_gyro/js/ops.js
+++ txt_gyro/js/ops.js
@@ -6,6 +6,7 @@ Ops.Exp=Ops.Exp || {};
 Ops.Html=Ops.Html || {};
 Ops.Exp.Gl=Ops.Exp.Gl || {};
 Ops.Sidebar=Ops.Sidebar || {};
+Ops.Gl.Matrix=Ops.Gl.Matrix || {};
 Ops.Gl.Meshes=Ops.Gl.Meshes || {};
 
 //----------------
@@ -1106,6 +1107,173 @@ function removeElementFromDOM(el) {
 };
 
 Ops.Sidebar.Sidebar.prototype = new CABLES.Op();
+
+//----------------
+
+
+
+// **************************************************************
+// 
+// Ops.Gl.Matrix.Transform
+// 
+// **************************************************************
+
+Ops.Gl.Matrix.Transform = function()
+{
+CABLES.Op.apply(this,arguments);
+var op=this;
+var attachments={};
+const render=op.addInPort(new CABLES.Port(op,"render",CABLES.OP_PORT_TYPE_FUNCTION));
+const trigger=op.addOutPort(new CABLES.Port(op,"trigger",CABLES.OP_PORT_TYPE_FUNCTION));
+
+
+const posX=op.inValue("posX",0);
+const posY=op.inValue("posY",0);
+const posZ=op.inValue("posZ",0);
+
+const scale=op.inValue("scale",0);
+
+const rotX=op.inValue("rotX",0);
+const rotY=op.inValue("rotY",0);
+const rotZ=op.inValue("rotZ",0);
+
+
+op.setPortGroup([rotX,rotY,rotZ]);
+op.setPortGroup([posX,posY,posZ]);
+
+
+var cgl=op.patch.cgl;
+var vPos=vec3.create();
+var vScale=vec3.create();
+var transMatrix = mat4.create();
+mat4.identity(transMatrix);
+
+var doScale=false;
+var doTranslate=false;
+
+var translationChanged=true;
+var scaleChanged=true;
+var rotChanged=true;
+
+scale.setUiAttribs({"divider":true});
+
+render.onTriggered=function()
+{
+    var updateMatrix=false;
+    if(translationChanged)
+    {
+        updateTranslation();
+        updateMatrix=true;
+    }
+    if(scaleChanged)
+    {
+        updateScale();
+        updateMatrix=true;
+    }
+    if(rotChanged)
+    {
+        updateMatrix=true;
+    }
+    if(updateMatrix)doUpdateMatrix();
+
+    cgl.pushModelMatrix();
+    mat4.multiply(cgl.mMatrix,cgl.mMatrix,transMatrix);
+
+    trigger.trigger();
+    cgl.popModelMatrix();
+
+    if(CABLES.UI && gui.patch().isCurrentOp(op))
+        gui.setTransformGizmo(
+            {
+                posX:posX,
+                posY:posY,
+                posZ:posZ,
+            });
+
+
+};
+
+op.transform3d=function()
+{
+    return {
+            pos:[posX,posY,posZ]
+        };
+
+};
+
+var doUpdateMatrix=function()
+{
+    mat4.identity(transMatrix);
+    if(doTranslate)mat4.translate(transMatrix,transMatrix, vPos);
+
+    if(rotX.get()!==0)mat4.rotateX(transMatrix,transMatrix, rotX.get()*CGL.DEG2RAD);
+    if(rotY.get()!==0)mat4.rotateY(transMatrix,transMatrix, rotY.get()*CGL.DEG2RAD);
+    if(rotZ.get()!==0)mat4.rotateZ(transMatrix,transMatrix, rotZ.get()*CGL.DEG2RAD);
+
+    if(doScale)mat4.scale(transMatrix,transMatrix, vScale);
+    rotChanged=false;
+};
+
+function updateTranslation()
+{
+    doTranslate=false;
+    if(posX.get()!==0.0 || posY.get()!==0.0 || posZ.get()!==0.0) doTranslate=true;
+    vec3.set(vPos, posX.get(),posY.get(),posZ.get());
+    translationChanged=false;
+}
+
+function updateScale()
+{
+    doScale=false;
+    if(scale.get()!==0.0)doScale=true;
+    vec3.set(vScale, scale.get(),scale.get(),scale.get());
+    scaleChanged=false;
+}
+
+var translateChanged=function()
+{
+    translationChanged=true;
+};
+
+var scaleChanged=function()
+{
+    scaleChanged=true;
+};
+
+var rotChanged=function()
+{
+    rotChanged=true;
+};
+
+
+rotX.onChange=rotChanged;
+rotY.onChange=rotChanged;
+rotZ.onChange=rotChanged;
+
+scale.onChange=scaleChanged;
+
+posX.onChange=translateChanged;
+posY.onChange=translateChanged;
+posZ.onChange=translateChanged;
+
+rotX.set(0.0);
+rotY.set(0.0);
+rotZ.set(0.0);
+
+scale.set(1.0);
+
+posX.set(0.0);
+posY.set(0.0);
+posZ.set(0.0);
+
+doUpdateMatrix();
+
+
+
+
+};
+
+Ops.Gl.Matrix.Transform.prototype = new CABLES.Op();
 
 //----------------
 
