1890c4359d074954cb2cfcf8490b2d4268ae63cc
Kristjan Dekleva <you@example.com>
2018-11-19 13:29:13 +0100

    lin1

---
+++ lin/js/index.js
@@ -0,0 +1,76 @@
+const line = new LeaderLine(
+  document.querySelector('.box-1'),
+  document.querySelector('.box-2'),
+    { 
+      size: 1,
+     path : 'straight',
+     color : 'grey',
+     startPlug : 'disc',
+     endPlug : 'disc',
+      startSocket: 'right', 
+      endSocket: 'left'
+  }
+
+);
+
+const line2 = new LeaderLine(
+  document.querySelector('.box-1'),
+  document.querySelector('.box-3'), 
+    { 
+      size: 1,
+     path : 'straight',
+     color : 'grey',
+     startPlug : 'disc',
+     endPlug : 'disc',
+      startSocket: 'right', 
+      endSocket: 'left'
+  }
+
+);
+
+const line3 = new LeaderLine(
+  document.querySelector('.box-2'),
+  document.querySelector('.box-4'), 
+    { 
+      size: 1,
+     path : 'straight',
+     color : 'grey',
+     startPlug : 'disc',
+     endPlug : 'disc',
+      startSocket: 'right', 
+      endSocket: 'left'
+  }
+
+)
+
+const line4 = new LeaderLine(
+  document.querySelector('.box-5'),
+  document.querySelector('.box-4'), 
+    { 
+      size: 1,
+     path : 'straight',
+     color : 'grey',
+     startPlug : 'disc',
+     endPlug : 'disc',
+      startSocket: 'right', 
+      endSocket: 'left'
+  }
+
+)
+
+
+
+addEventListener('scroll', AnimEvent.add(function() {
+  if (line) { line.position(); }
+}), true);
+
+addEventListener('scroll', AnimEvent.add(function() {
+  if (line2) { line2.position(); }
+}), true);
+addEventListener('scroll', AnimEvent.add(function() {
+  if (line3) { line3.position(); }
+}), true);
+
+addEventListener('scroll', AnimEvent.add(function() {
+  if (line4) { line4.position(); }
+}), true);
